ec3a4d3ddb92f4b648cc4576d6b8aae1bc761b82
kwabe007 <kwabe007@users.noreply.github.com>
2024-06-10 17:07:44 +0200

    Add meta function to root

--- app/root.tsx
+++ app/root.tsx
@@ -2,9 +2,14 @@ import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/reac
 import './tailwind.css'
 import { json, LoaderFunctionArgs } from '@remix-run/node'
 import { getUser } from '~/session.server'
+import type { MetaFunction } from '@remix-run/node'
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   return json({ user: await getUser(request) })
+}
+
+export const meta: MetaFunction = () => {
+  return [{ title: 'New Remix App' }, { name: 'description', content: 'Welcome to Remix!' }]
 }
 
 export function Layout({ children }: { children: React.ReactNode }) {
