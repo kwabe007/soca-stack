ade0f9487c751e05118b39e77e02fc2232d0f463
kwabe007 <kwabe007@users.noreply.github.com>
2024-10-01 01:40:17 +0200

    Update user-utils.ts

--- app/domains/users/user-utils.ts
+++ app/domains/users/user-utils.ts
@@ -1,5 +1,5 @@
 import { Role, User } from '~/domains/users/user-schema'
 
-export function isAdmin(user: User) {
-  return user.role === Role.Admin
+export function isAdmin(user: User | undefined) {
+  return user?.role === Role.Admin
 }
